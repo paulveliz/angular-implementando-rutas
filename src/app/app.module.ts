493117287d0dd921d4ff3605b416c11325c1f20d
Paul guadalupe veliz lopez <paulsystem32@gmail.com>
2021-02-08 14:52:23 -0700

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { PaginaInicioComponent } from './pagina-inicio/pagina-inicio.component';
+import { ListaCarrosComponent } from './lista-carros/lista-carros.component';
+import { AcercaDeComponent } from './acerca-de/acerca-de.component';
+import { RouterModule, Routes } from '@angular/router';
+import { Error404Component } from './error404/error404.component';
+
+const myRoutes:Routes = [ 
+  { path: 'acerca', component: AcercaDeComponent },
+  { path: '404', component: Error404Component },
+  { path: 'lista', component: ListaCarrosComponent },
+  { path: '', component: PaginaInicioComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    PaginaInicioComponent,
+    ListaCarrosComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/lista-carros/lista-carros.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-carros',
+  templateUrl: './lista-carros.component.html',
+  styleUrls: ['./lista-carros.component.css']
+})
+export class ListaCarrosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
